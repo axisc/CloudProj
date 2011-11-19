f7207c398891b7375885bb765f09c56881c0c967
Kannapiran Thirunarayanan <kanap008@gmail.com>
2011-11-18 21:40:49 -0500

    linked in API working properly

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+<head>
+
+<script type="text/javascript">
+	function goToNextPage(){
+		location.href = "linkedInSignIn.php";
+	}
+
+//	LinkedIn
+//	api key: rbjecyqtr9pn
+//	secret key: c3Z22HhLxFY6FUIB
+</script>
+
+</head>
+
+<body>
+
 <?php
 
 echo "Welcome to RECRU SOCIAL";
 
 ?>
+
+<form>
+	<input type="button" onclick="goToNextPage()" name="linkedIn" value="linded..in"/>
+</form>
+
+
+</body>
+
+</html>
