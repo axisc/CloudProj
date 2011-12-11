0ee82024a608c4196341516f7c6f359687c5cf9e
acs <acs@.(none)>
2011-12-11 17:26:42 -0500

    Stashing changes

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "Welcome to RECRU SOCIAL";
-
+echo "<h1>Welcome to RECRU SOCIAL</h1>";
+ $test ="http://www.gmail.com";
 ?>
+
+<h2> <?php echo "test header 2"; ?></h2>
+<h3> <?php echo $test ; ?></h3>
+<a href=<?php echo $test; ?>>gmail </a>
--- linkedInSignIn.php
+++ linkedInSignIn.php
@@ -12,19 +12,62 @@
 
 <script type="text/javascript">
 
-function loadData() {
-IN.API.Profile("me")
-   .result(function(result) { 
-      $("#profile").html('<script type="IN/FullMemberProfile" data-id="' + result.values[0].id + '"><script>');
-      IN.parse(document.getElementById("profile"))
-   })
+function loadData(){
+	IN.API.MemberUpdates("url=http://www.linkedin.com/in/kanap008")
+		.fields(["updateContent:(person:(headline,first-name,id))"])
+	.result(function(result){
+	   alert (JSON.stringify(result))
+	})
 }
+//function loadData() {
+//IN.API.Profile("me").fields(["firstName","headline","positions:(company)"])
+//   .result(function(result) { 
+//      $("#profile").html('<script type="IN/FullMemberProfile" data-id="' + result.values[0].id + '"><script>');
+//      IN.parse(document.getElementById("profile"))
+//   })
+//}
+
+function getMemberUpdates(){
+	IN.API.MemberUpdates("me")
+	.fields(["isLiked","updateContent:(person:(headline,first-name))"])
+	.result(function(result){
+		
+	})
+}
+
+
+function displayNetworkUpdates(updates) {
+	  var profileDiv = document.getElementById("profile");
+	     
+	  for (var i in updates.values) {
+	    var key = updates.values[i].updateKey; // unique key used to reference <p> in Raw API call results
+	    var share = updates.values[i].updateContent.person; // person making update
+	    profileDiv.innerHTML += "<p id='" + key + "'>" + share.firstName + " " + share.lastName
+	      + " shared " + share.currentShare.comment + ".</p>";
+
+	    IN.API.Raw("/people/~/network/updates/key=" + key + "/likes") // construct REST URL
+	      .result( function(value) { return function(likes) { // need to wrap inside function to get proper key
+	        document.getElementById(value).innerHTML += " (Liked by " + likes._total + " people)";
+	       } }(key))
+	      .error( function(error) { /* do nothing */ } );
+	  }
+	}
+
+//function getProfile(){
+//	IN.API.Profile("me")
+//	   .result(function(result) { 
+//	      $("#profile").html('<script type="IN/FullMemberProfile" data-id="Hv3V1lv4SQ" data-firstName="Nikhil" data-lastName="Bhagwat"><script>');
+//	      IN.parse(document.getElementById("name"))
+//	   })
+//}
 
 </script>
 
 </head>
+
 <body class="yui3-skin-sam     yui-skin-sam">
 <div id="profile"></div>
+
 <script type="IN/Login" data-onAuth="loadData"></script>
 </body>
 </html>
